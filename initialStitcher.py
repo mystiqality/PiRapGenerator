36c6beac75df3178dc927ddb1b7da6ded56a8003
mystiqality <97352617+mystiqality@users.noreply.github.com>
2022-01-08 16:14:41 +0000

    Add files via upload

---
+++ initialStitcher.py
@@ -0,0 +1,68 @@
+from pydub import AudioSegment
+import time as t
+
+piRendered = ""
+total = 0
+
+## USER MODIFIABLE VALUES ##
+
+chunkSize = 1000
+piTxtName = "pi250k.txt"
+
+pi0 = AudioSegment.from_wav("pi0.wav")
+pi1 = AudioSegment.from_wav("pi1.wav")
+pi2 = AudioSegment.from_wav("pi2.wav")
+pi3 = AudioSegment.from_wav("pi3.wav")
+pi4 = AudioSegment.from_wav("pi4.wav")
+pi5 = AudioSegment.from_wav("pi5.wav")
+pi6 = AudioSegment.from_wav("pi6.wav")
+pi7 = AudioSegment.from_wav("pi7.wav")
+pi8 = AudioSegment.from_wav("pi8.wav")
+pi9 = AudioSegment.from_wav("pi9.wav")
+
+## END ##
+
+
+
+
+pi = open(piTxtName, "r").read()
+digits = {"0":pi0, "1":pi1, "2":pi2, "3":pi3, "4":pi4, "5":pi5, "6":pi6, "7":pi7, "8":pi8, "9":pi9}
+
+try:
+    for char in pi:
+        # if first character, create variables
+        if(len(piRendered) == 0):
+            song = digits[char]
+            piRendered = char
+            total += 1
+            
+        # if chunk size met, export audio then create new chunk
+        elif(len(piRendered) % chunkSize == 0):
+            name =  "piRap" + str(total) + ".wav"
+                        
+            song.export(name, format="wav")
+                        
+            print("Saved file: " + name)
+                        
+            song = digits[char]
+            piRendered = char
+            total += 1
+        
+        else:
+            piRendered += char
+            song += digits[char]
+
+            total += 1
+            print(str(len(piRendered)) + " - " + str(total))
+            
+
+
+except:
+    print("\nStopping")
+    pass
+
+
+
+
+song.export("piRapFail.wav", format="wav")
+t.sleep(5)
